1424cfb0dcbf9ef5c5c238f9ce4f2a8ef975ac79
3096 <contact@the3096.com>
2023-05-11 19:43:45 -0700

    add theme, header, footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,31 @@
 <script lang="ts">
   // The ordering of these imports is critical to your app working properly
-  import '@skeletonlabs/skeleton/themes/theme-rocket.css';
+  import '../theme.postcss';
   // If you have source.organizeImports set to true in VSCode, then it will auto change this ordering
   import '@skeletonlabs/skeleton/styles/skeleton.css';
   // Most of your app wide CSS should be put in this file
   import '../app.postcss';
+
+  import { AppBar, AppShell } from '@skeletonlabs/skeleton';
+  import Logo from '$lib/components/Logo.svelte';
 </script>
 
-<slot />
+<AppShell>
+  <!-- <svelte:fragment slot="header">Header</svelte:fragment> -->
+  <!-- <svelte:fragment slot="sidebarLeft">Sidebar Left</svelte:fragment> -->
+  <!-- (sidebarRight) -->
+  <AppBar slot="pageHeader" gridColumns="1" class=" pb-[1.5]">
+    <div class="flex">
+      <Logo />
+    </div>
+  </AppBar>
+  <!-- Router Slot -->
+  <slot />
+  <!-- ---- / ---- -->
+  <AppBar slot="pageFooter" gridColumns="1">
+    <div class="flex justify-center">
+      <span>Copyright © 2023 Gamementship</span>
+    </div>
+  </AppBar>
+  <!-- (footer) -->
+</AppShell>
