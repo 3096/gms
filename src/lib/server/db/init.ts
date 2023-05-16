992a1f3afb2382d447d1a9320741f46f8c1d64d4
3096 <contact@the3096.com>
2023-05-16 00:15:46 -0700

    fix env for prod

--- src/lib/server/db/init.ts
+++ src/lib/server/db/init.ts
@@ -6,18 +6,17 @@ import { createUserGameRelation } from './user_game';
 import { testDB } from './initData';
 
 export const initDb = async () => {
-  const sql = pg(dev ? import.meta.env.VITE_DATABASE_URL : import.meta.env.DATABASE_URL);
+  const sql = pg(dev ? import.meta.env.VITE_DATABASE_URL : process.env.DATABASE_URL);
   try {
     const [{ exists }] = await sql`SELECT EXISTS (
       SELECT datname FROM pg_catalog.pg_database WHERE datname = ${sql.options.database}
       )`;
     // only run this if the database doesn't exist cuz idk what to do if it does
-    // if (exists) {
-    //   // return sql;
-    // }
-    // console.log("here");
+    if (exists) {
+      return sql;
+    }
   } catch (e) {
-    if (e.code === '3D000') {
+    if ((e as { code: string }).code === '3D000') {
       await pg(
         // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
         `postgres://${sql.options.user}:${sql.options.pass!}@${sql.options.host[0]}:${
