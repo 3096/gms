fdc9d8e00bdf4eb653de1b6c17881a51146bf7c5
3096 <contact@the3096.com>
2023-05-15 23:35:33 -0700

    add redirect

--- src/routes/users/new/+page.svelte
+++ src/routes/users/new/+page.svelte
@@ -1,20 +1,22 @@
 <script>
+  import { goto } from '$app/navigation';
   import ValidForm from '$lib/components/forms/ValidForm.svelte';
   import ValidInput from '$lib/components/forms/ValidInput.svelte';
   import { formSchema } from './form';
 
-  // TODO: onSuccessfulSubmit
+  let username = '';
 </script>
 
 <div class="card max-w-md mx-auto my-10 p-5 rounded-md shadow-sm">
   <h1 class="text-2xl font-medium mb-5">Join GMS</h1>
-  <ValidForm submitText="Sign Up" {formSchema}>
+  <ValidForm submitText="Sign Up" {formSchema} onSuccess={() => goto(`/user/${username}`)}>
     <ValidInput
       label="Choose a username"
       type="text"
       name="username"
       placeholder="EpicLlama420"
       required
+      bind:value={username}
     />
   </ValidForm>
 </div>
